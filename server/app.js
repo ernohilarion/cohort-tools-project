c4350d8c58214a2b443e43b7bf1a39f095836b59
Rodolfo Bittkau <tyrmund@gmail.com>
2024-05-21 17:06:18 +0200

    database and server connected

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 const cors = require("cors");
+const mongoose = require("mongoose")
+const Cohort = require("./models/cohort.model")
+const Student = require("./models/student.model")
 
 // STATIC DATA
 
@@ -11,7 +14,6 @@ const studentsData = require("./students.json")
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -25,9 +27,15 @@ app.use(cors({
   origin: ['http://localhost:5005', 'http://localhost:5173']
 }))
 
-// ROUTES - https://expressjs.com/en/starter/basic-routing.html
-// Devs Team - Start working on the routes here:
-// ...
+// DATABASE & MODELS
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to MongoDB", err));
+
+// ROUTES
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
@@ -40,15 +48,31 @@ app.get("/api/students", (req, res) => {
   res.json(studentsData)
 });
 
+app.get("/cohorts", (req, res) => {
 
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieved cohorts ->", cohorts)
+      res.json(cohorts)
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts ->", error)
+      res.status(500).json({ error: "Failed to retrieve cohorts" })
+    })
+})
 
+app.get("/students", (req, res) => {
 
-
-
-
-
-
-
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieved students ->", students)
+      res.json(students)
+    })
+    .catch((error) => {
+      console.error("Error while retrieving students ->", error)
+      res.status(500).json({ error: "Failed to retrieve students" })
+    })
+})
 
 // START SERVER
 app.listen(PORT, () => {
